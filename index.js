3184a5e0896762b3e1c4494d87466c9e2d574a6e
nocturnalplay <nocturnalplay0@gmail.com>
2023-06-15 18:49:06 +0530

    updated all the three annot wf plc data under the limit

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-app.get('/data', (req, res) => {
-  // Read the output.json file
-  const output = require('./output.json');
-
-  // Extract the data array from the output object
-  const dataArray = output.data[1].data;
-
+app.get("/data", (req, res) => {
   // Get the limit from the URL parameter, or set a default value
   const limit = parseInt(req.query.limit) || 1000;
 
+  // Read the output.json file
+  let output = require("./output.json");
+  //sliced only three data ---- annot,wf,plc
+  output.data = output.data.slice(0, 3); //[ 'annot', 'wf', 'plc' ]
+
+  // Extract the data array from the output object
+  const wf = output.data[1].data;
+
+  // Extract the data array from the output object
+  const plc = output.data[2].data;
+
   // Create a new limitedData array with the desired size
-  const limitedData = dataArray.slice(0, limit);
+  const wflimit = wf.slice(0, limit);
+  // Create a new limitedData array with the desired size
+  const plclimit = plc.slice(0, limit);
+
+  output.data[1].data = wflimit;
+  output.data[2].data = plclimit;
 
   // Send the limitedData as the response
-  res.json({ data: limitedData });
+  res.json({ data: output.data });
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
